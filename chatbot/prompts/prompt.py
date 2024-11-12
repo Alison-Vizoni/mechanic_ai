61e2810d481c44377d7e588900458fd7cabebc26
Alison-Vizoni <alison.vizoni@gmail.com>
2024-11-11 22:38:23 -0300

    feat: add prompt content

--- chatbot/prompts/prompt.py
+++ chatbot/prompts/prompt.py
@@ -1,8 +1,10 @@
 from os import path
 
+from config import settings
+
 
 def get_prompt():
-    directory = path.join('files', 'default_prompt.txt')
+    directory = path.join(settings.BASE_DIR, 'chatbot', 'prompts', 'files', 'default_prompt.txt')
     with open(directory) as f:
         file = f.read()
     return file
