7ecdd3f849b2f72033ed977db79609c2eabb0d8e
Alison-Vizoni <alison.vizoni@gmail.com>
2024-11-11 22:39:02 -0300

    fix: change redis to PostgresDB

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,13 +1,12 @@
 from typing import Literal
 from uuid import uuid4
 
-from langchain.chains.conversation.base import ConversationChain
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_groq import ChatGroq
+from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.constants import START, END
 from langgraph.graph import StateGraph, MessagesState
 
-from chatbot.db import MemoryDB
 from chatbot.prompts.prompt import get_prompt
 from config import settings
 
@@ -17,19 +16,20 @@ class Chatbot:
         self.chatbot_model_id = settings.CHATBOT_MODEL_ID
         self.model = ChatGroq(api_key=settings.API_KEY, model_name=self.chatbot_model_id, temperature=0)
         self.state_graph = StateGraph(state_schema=MessagesState)
-        self.memory = MemoryDB('session_1')
         self.system_prompt = get_prompt()
 
-        self.conversation_chain = ConversationChain()
-
     def ask_question(self, question):
-        app = self._get_app()
+        self._add_app_config()
+        with PostgresSaver.from_conn_string(settings.DB_URL) as checkpointer:
+            checkpointer.setup()
+            app = self.state_graph.compile(checkpointer=checkpointer)
 
-        system_message = self._get_system_message()
-        input_message = HumanMessage(content=question)
-        config = {"configurable": {"thread_id": uuid4()}}
+            system_message = self._get_system_message()
+            input_message = HumanMessage(content=question)
+            config = {'configurable': {'thread_id': str(uuid4())}}
+            input_message = {'messages': [system_message, input_message]}
 
-        final_state = app.invoke(input=[system_message, input_message], config=config)
+            final_state = app.invoke(input=input_message, config=config)
 
         response_message = final_state.get('messages', [None])[-1]
         if response_message and hasattr(response_message, 'content'):
@@ -49,14 +49,11 @@ class Chatbot:
             return 'tools'
         return END
 
-    def _get_app(self):
+    def _add_app_config(self):
         self.state_graph.add_node('agent', self.call_model)
         self.state_graph.add_edge(START, 'agent')
         self.state_graph.add_conditional_edges('agent', self.should_continue)
-        return self.state_graph.compile(checkpointer=self.memory)
 
     def _get_system_message(self):
-        content = [
-            f'Template: {self.system_prompt}',
-        ]
+        content = f'Template: {self.system_prompt}'
         return SystemMessage(content=content)
--- chatbot/db.py
+++
@@ -1,9 +0,0 @@
-from langchain.memory.chat_message_histories import RedisChatMessageHistory
-
-from config import settings
-
-
-class MemoryDB:
-    def __init__(self, session_id):
-        url = f'redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/0'
-        self.redis_client = RedisChatMessageHistory(url=url, session_id=session_id)
